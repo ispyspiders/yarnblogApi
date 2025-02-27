4c98d549f31ef2585e56683b31cf6703900ba398
Kajsa Classon <ispyspiders@gmail.com>
2025-02-27 14:10:55 +0100

    Funktionalitet för att registrera, logga in och ut användare

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    // Registrera användare
+    public function register(Request $request)
+    {
+        $validatedUser = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required'
+            ]
+        );
+
+        // Misslyckad validering
+        if ($validatedUser->fails()) {
+            return response()->json([
+                'message' => 'Validation error',
+                'error' => $validatedUser->errors()
+            ], 401);
+        }
+
+        // Lyckad validering - lagra användare och returnera token
+        $user = User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => bcrypt($request['password'])
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $response = [
+            'message' => 'User created successfully',
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201); // return response created
+    }
+
+    // Logga in användare
+    public function login(Request $request)
+    {
+        $validatedUser = Validator::make(
+            $request->all(),
+            [
+                'email' => 'required|email',
+                'password' => 'required'
+            ]
+        );
+
+        // Misslyckad validering
+        if ($validatedUser->fails()) {
+            return response()->json([
+                'message' => 'Validation error',
+                'error' => $validatedUser->errors()
+            ], 401);
+        }
+
+        // Felaktig credentials
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'error' => 'Failed credentials',
+                'message' => 'Incorrect email and/or password'
+            ], 401);
+        }
+
+        // Korrekt inloggning - returnera token
+        $user = User::where('email', $request->email)->first();
+        return response()->json([
+            'message' => 'User logged in!',
+            'token' => $user->createToken('auth_token')->plainTextToken
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        $response = [
+            'message' => 'User logged out!'
+        ];
+        return response($response, 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Public routes
+Route::resource('posts', PostController::class); // Routing för inlägg
+Route::post('/register', [AuthController::class, 'register']); // Route för att registrera användare
+Route::post('/login', [AuthController::class, 'login']); // Route för att logga in användare
+
+// Route::get('/user/posts', []) // Route för att hämta en användares inlägg
+
+// Protected routes
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum'); // Logga ut användare
